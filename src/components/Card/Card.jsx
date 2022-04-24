b9e6a252fc0bbbcd343bc5ec02ce40df45bca0f0
ofego favour <ofegofavour@gmail.com>
2022-04-24 21:59:31 +0100

    Dashboard

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,55 +1,57 @@
-// import React from 'react'
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import "./Card.css";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 import { motion, AnimateSharedLayout } from "framer-motion";
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
-import './Card.css';
 import { UilTimes } from "@iconscout/react-unicons";
 import Chart from "react-apexcharts";
+
+// parent Card
+
 const Card = (props) => {
-
-  const [expanded, setExpanded]= useState(false)
-
+  const [expanded, setExpanded] = useState(false);
   return (
-        <AnimateSharedLayout>
-        {expanded ? (
-          <ExpandedCard param={props} setExpanded={() => setExpanded(false)} />
-        ) : (
-          <CompactCard param={props} setExpanded={() => setExpanded(true)} />
-        )}
-      </AnimateSharedLayout>
-      )
-}
+    <AnimateSharedLayout>
+      {expanded ? (
+        <ExpandedCard param={props} setExpanded={() => setExpanded(false)} />
+      ) : (
+        <CompactCard param={props} setExpanded={() => setExpanded(true)} />
+      )}
+    </AnimateSharedLayout>
+  );
+};
 
-//CompactCard
-function CompactCard ({param, setExpanded}){
+// Compact Card
+function CompactCard({ param, setExpanded }) {
   const Png = param.png;
-  return(
-    <div className="CompactCard" 
-        style={{
-          background: param.color.backGround,
-          boxShadow: param.color.boxShadow,
-        }}
-        onClick={setExpanded}
-      >
-        <div className="radialBar">
+  return (
+    <motion.div
+      className="CompactCard"
+      style={{
+        background: param.color.backGround,
+        boxShadow: param.color.boxShadow,
+      }}
+      layoutId="expandableCard"
+      onClick={setExpanded}
+    >
+      <div className="radialBar">
         <CircularProgressbar
           value={param.barValue}
           text={`${param.barValue}%`}
         />
         <span>{param.title}</span>
-        </div>
-         <div className="detail">
-             <Png/>
-             <span>${param.value}</span>
-              <span>last 24 hours</span>
-         </div>
-    </div>
-  )
+      </div>
+      <div className="detail">
+        <Png />
+        <span>${param.value}</span>
+        <span>Last 24 hours</span>
+      </div>
+    </motion.div>
+  );
 }
 
-//ExpordedCard
-function ExpandedCard({param,setExpanded}){
+// Expanded Card
+function ExpandedCard({ param, setExpanded }) {
   const data = {
     options: {
       chart: {
@@ -100,24 +102,26 @@ function ExpandedCard({param,setExpanded}){
       },
     },
   };
-   return(
-       <div
-       className="ExpandedCard"
-       style={{
-         background: param.color.backGround,
-         boxShadow: param.color.boxShadow,
-       }}
-       layoutId="expandableCard"
-     >
-       <div style={{ alignSelf: "flex-end", cursor: "pointer", color: "white" }}>
-         <UilTimes onClick={setExpanded} />
-       </div>
-         <span>{param.title}</span>
-       <div className="chartContainer">
-         <Chart series={param.series} type='area'/>
-       </div>
-       <span>Last 24 hours</span>
-     </div>
-   )
+
+  return (
+    <motion.div
+      className="ExpandedCard"
+      style={{
+        background: param.color.backGround,
+        boxShadow: param.color.boxShadow,
+      }}
+      layoutId="expandableCard"
+    >
+      <div style={{ alignSelf: "flex-end", cursor: "pointer", color: "white" }}>
+        <UilTimes onClick={setExpanded} />
+      </div>
+        <span>{param.title}</span>
+      <div className="chartContainer">
+        <Chart options={data.options} series={param.series} type="area" />
+      </div>
+      <span>Last 24 hours</span>
+    </motion.div>
+  );
 }
-export default Card
+
+export default Card;
