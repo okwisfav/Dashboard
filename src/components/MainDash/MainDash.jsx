f60be237ca394e0400ac7a6c917e9364281aa5b0
ofego favour <ofegofavour@gmail.com>
2022-04-25 09:31:04 +0100

    Dashboard

--- src/components/MainDash/MainDash.jsx
+++ src/components/MainDash/MainDash.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Cards from '../Cards/Cards'
+import Table from '../Table/Table'
 import './MainDash.css'
 const MainDash = () => {
   return (
       <div className="MainDsh">
           <h1>Dashboard</h1>
           <Cards/>
+          <Table/>
       </div>
   )
 }
---
+++ src/components/RigtSide/RightSide.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './RightSide.css'
+const RightSide = () => {
+  return (
+    <div>RightSide</div>
+  )
+}
+
+export default RightSide
